45719dd60ba69b657ffb3a9a1a95ec1e5c8fb2be
Léa Eponville <lea.eponville@gmail.com>
2024-06-20 19:20:23 +0200

    first commit

---
+++ .angular/cache/18.0.3/vite/deps/@angular_cdk_layout.js
@@ -0,0 +1,16 @@
+import {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+  MediaMatcher
+} from "./chunk-6MDWO5LW.js";
+import "./chunk-UAV676AC.js";
+import "./chunk-P4E4X5Y5.js";
+import "./chunk-I7PV7N6Y.js";
+export {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+  MediaMatcher
+};
+//# sourceMappingURL=@angular_cdk_layout.js.map
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NewnavbarComponent } from './newnavbar/newnavbar.component';
+import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NewnavbarComponent, MatSlideToggleModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular_fil-rouge';
+}
