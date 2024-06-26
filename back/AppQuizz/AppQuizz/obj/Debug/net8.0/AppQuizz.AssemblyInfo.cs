eadb558222caa429c57d7682ba77e39ac33287c6
Waren <warenpoyer@gmail.com>
2024-06-26 16:10:38 +0200

    test

--- back/AppQuizz/AppQuizz/obj/Debug/net8.0/AppQuizz.AssemblyInfo.cs
+++ back/AppQuizz/AppQuizz/obj/Debug/net8.0/AppQuizz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppQuizz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a055d0d16e186102036e55474163aa9323ef52df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a774ba0b97de5ee775a2b8267e2bac48c2495b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppQuizz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppQuizz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
