9b770bd62d216b2797fade9c190f973af40d2e6d
yousseflim <yousseflimem9@gmail.com>
2024-06-28 12:22:51 +0200

    sidebar

--- front/src/app/pages/admin/admin.component.ts
+++ front/src/app/pages/admin/admin.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
-
+  links = [
+    { label: "Accueil", path: "/" },
+    { label: "Générer un nouveau quiz", path: "/generate-quiz" },
+    { label: "Configurer les niveaux d'expérience", path: "/config-exp" },
+    { label: "Création et édition de technologies", path: "/edit-techno" },
+    { label: "Gestion des agents", path: "/manage-agent" },
+    { label: "Gérer le référentiel de questions et réponses", path: "questions-answers-referential" },
+    { label: "Voir les quiz", path: "see-quiz" }
+  ];
 }
 
--- front/src/app/pages/agent/agent.component.ts
+++ front/src/app/pages/agent/agent.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar';
 
 @Component({
   selector: 'app-agent',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './agent.component.html',
   styleUrl: './agent.component.css'
 })
 export class AgentComponent {
-
+  links = [
+    { label: "Accueil", path: "/" },
+    { label: "Générer un nouveau quiz", path: "/generate-quiz" },
+    { label: "Lister les quiz", path: "/list-quiz" },
+    { label: "Résultats", path: "/results" }
+  ];
 }
