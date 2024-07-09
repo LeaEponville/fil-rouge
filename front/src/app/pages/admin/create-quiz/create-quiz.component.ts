6743e0477d4681e157c62d92d6296e7c43ed390a
Léa <lea.eponville@gmail.com>
2024-07-09 10:36:05 +0200

    changed 'edit-techno' to 'create-quiz' and button 'générer le quiz' works

--- front/src/app/pages/admin/create-quiz/create-quiz.component.ts
+++ front/src/app/pages/admin/create-quiz/create-quiz.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from '../../../components/sidebar/sidebar';
 
 @Component({
-  selector: 'app-edit-techno',
+  selector: 'app-create-quiz',
   standalone: true,
   imports: [SidebarComponent],
-  templateUrl: './edit-techno.component.html',
-  styleUrl: './edit-techno.component.css'
+  templateUrl: './create-quiz.component.html',
+  styleUrl: './create-quiz.component.css'
 })
-export class EditTechnoComponent {  
+export class CreateQuizComponent {  
   links = [
     { label: "Accueil", path: "/" },
     { label: "Générer un nouveau quiz", path: "/generate-quiz-admin" },
     /*{ label: "Configurer les niveaux d'expérience", path: "/config-exp" },*/
-    { label: "Création et édition de technologies", path: "/edit-techno" },
+    { label: "Créer un quiz", path: "/create-quiz" },
     { label: "Gestion des agents", path: "/manage-agent" },
     /*{ label: "Gérer le référentiel de questions et réponses", path: "questions-answers-referential" },*/
     { label: "Voir les quiz", path: "see-quiz" }
